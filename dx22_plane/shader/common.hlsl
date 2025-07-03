b4b60f069f0c949f12d26b1846064dbf857dc937
辻省吾 <tsuji.shogo.30047@gmail.com>
2025-07-03 17:32:41 +0900

    セットアップその４

---
+++ dx22_plane/shader/common.hlsl
@@ -0,0 +1,77 @@
+//cbuffer WorldBuffer : register(b0)
+//{
+//	matrix World;
+//}
+cbuffer ViewBuffer : register(b1)
+{
+	matrix View;
+}
+cbuffer ProjectionBuffer : register(b2)
+{
+	matrix Projection;
+}
+
+// 定数バッファ受け取り
+cbuffer ConstBuffer : register(b0) 
+{
+	// 頂点カラー
+	float4 vertexColor;
+
+	// UV座標移動行列
+	matrix matrixTex1;
+
+	// プロジェクション変換行列
+	matrix matrixProj;
+
+	// ワールド変換行列
+	matrix matrixWorld;
+
+	// ビュー変換行列
+	matrix matrixView;
+}
+
+struct VS_IN
+{
+    float4 pos : POSITION0;
+	float4 nrm : NORMAL0;
+    float4 col : COLOR0;
+    float2 tex : TEXCOORD0;
+};
+
+struct PS_IN
+{
+	float4 pos : SV_POSITION;
+	float4 col : COLOR0;
+	float2 tex : TEXCOORD0;
+};
+
+struct LIGHT {
+	bool   Enable;		// 使用するか否か
+	bool3  Dummy;		// PADDING
+	float4 Direction;	// 方向
+	float4 Diffuse;		// 拡散反射用の光の強さ
+	float4 Ambient;		// 環境光用の光の強さ
+};
+
+cbuffer LightBuffer:register(b3) {
+	LIGHT Light;
+}
+
+struct MATERIAL {
+	float4 Ambient;
+	float4 Diffuse;
+	float4 Specular;
+	float4 Emission;
+	float Shininess;
+	bool TextureEnable;
+	bool2 Dummy;
+};
+
+cbuffer MaterialBuffer:register(b4) {
+	MATERIAL Material;
+}
+
+// ＵＶ座標移動行列
+cbuffer MaterialBuffer : register(b5) {
+	matrix matrixTex;
+}
---
+++ dx22_plane/shader/unlitTextureVS.hlsl
@@ -0,0 +1,43 @@
+#include "common.hlsl"
+
+PS_IN vs_main(in VS_IN input)
+{
+    PS_IN output;
+	
+	//matrix wvp;
+	//wvp = mul(World, View);
+	//wvp = mul(wvp, Projection);
+
+ //   output.pos = mul(input.pos, wvp);
+
+	//// ‚t‚uÀ•W‚ğˆÚ“®‚³‚¹‚é
+	//float4 uv;
+	//uv.xy = input.tex;	// s—ñŠ|‚¯Z‚Ì‚½‚ßflaot4Œ^‚ÉˆÚ‚·
+	//uv.z = 0.0f;
+	//uv.w = 1.0f;
+	//uv = mul(uv, matrixTex);	// ‚t‚uÀ•W‚ÆˆÚ“®s—ñ‚ğŠ|‚¯Z
+	//output.tex = uv.xy;			// Š|‚¯Z‚ÌŒ‹‰Ê‚ğ‘—M—p•Ï”‚ÉƒZƒbƒg
+
+	////output.tex = input.tex;
+ //   output.col = input.col;
+	//
+
+	output.pos = mul(input.pos, matrixWorld);
+
+	output.pos = mul(output.pos, View);
+
+	output.pos = mul(output.pos, Projection);
+
+	// ‚t‚uÀ•W‚ğˆÚ“®‚³‚¹‚é
+	float4 uv;
+	uv.xy = input.tex;	// s—ñŠ|‚¯Z‚Ì‚½‚ßflaot4Œ^‚ÉˆÚ‚·
+	uv.z = 0.0f;
+	uv.w = 1.0f;
+//	uv = mul(uv, matrixTex);	// ‚t‚uÀ•W‚ÆˆÚ“®s—ñ‚ğŠ|‚¯Z
+	output.tex = uv.xy;			// Š|‚¯Z‚ÌŒ‹‰Ê‚ğ‘—M—p•Ï”‚ÉƒZƒbƒg
+
+    output.col = input.col * vertexColor;
+
+    return output;
+}
+
