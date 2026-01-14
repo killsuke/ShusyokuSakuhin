b32fb75789dec27ac7d0802afa51f8a90d1a1730
辻省吾 <tsuji.shogo.30047@gmail.com>
2026-01-14 14:06:01 +0900

    グロー用のシェーダー改善
    ・グロー用のシェーダーの不要なコードを削除

--- dx22_plane/shader/unlitLuminescencePS.hlsl
+++ dx22_plane/shader/unlitLuminescencePS.hlsl
@@ -3,26 +3,26 @@
 // ピクセルシェーダーのエントリポイント
 float4 ps_main(in PS_IN input) : SV_Target
 {
-    float4 color;
-
     float3 BaseColor = input.col.rgb; // 通常色
     
+    // uvの中心を基準に距離を計算
     float2 uv = input.tex;
     float2 d = uv - 0.5f;
     
     float s, c;
-    sincos(Angle, s, c); // 回転を使う
+    sincos(Angle, s, c); // 回転を使うので行列生成
     float2x2 rotationMtx = float2x2(c, -s, s, c);
     d = mul(rotationMtx, d);
-        
-    float2 scale = EllipseScale;
-    d *= scale;
     
-    float dist = sqrt(dot(d, d));
+    // 楕円形にも対応できるようスケール設定
+    d *= EllipseScale;
+    
+    // 中心から離れれば離れるほどボヤける
+    float dist = dot(d, d);
     
     // グロー量（外に行くほど減衰）
-    float glow = saturate(1.0f - dist / GlowRadius);
-    glow = glow * glow; // 二乗して中心部を強調
+    float glow = saturate(1.0f - dist / (GlowRadius * GlowRadius));
+    glow *= glow; // 二乗して中心部を強調
     
     float3 colorRGB = BaseColor + GlowColor.rgb * glow * GlowPower;
     
